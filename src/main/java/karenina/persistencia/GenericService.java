cc6634e822c62b5016885a9649db5888721715fe
EXCC0100 <EXCC0100@DAVID-PEREZ.oficinas.sepg.minhac.age>
2018-01-15 18:19:04 +0100

    Se corrige un error

--- src/main/java/karenina/persistencia/GenericService.java
+++ src/main/java/karenina/persistencia/GenericService.java
@@ -1,8 +1,5 @@
 package karenina.persistencia;
 
-import org.springframework.stereotype.Service;
-
-@Service ("FooDao")
 public abstract class GenericService <T extends Entity> implements IFooService <T>{
  
    abstract void setDao(IGenericDao<T> daoToSet );
